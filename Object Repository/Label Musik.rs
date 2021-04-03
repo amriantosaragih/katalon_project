04d5744a4e6561eb286ac453413399d4a356a227
hanamnr <hana@mmi-pt.com>
2021-04-03 15:01:27 +0700

    Perubahan Line

---
+++ Object Repository/Label Musik.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Label Musik</name>
+   <tag></tag>
+   <elementGuidId>855cd528-74a8-4627-802e-20bf095a8b5a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@title=&quot;Music&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
